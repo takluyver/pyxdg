724e755486caba9d92e2829a9ebbfa15e0d517fd
Heinrich Wendel <h_wendel@cojobo.net>
2005-03-18 21:53:20 +0000

    ...

--- test/test-menu.py
+++ test/test-menu.py
@@ -7,7 +7,7 @@ import xdg.DesktopEntry
 
 def show_menu(menu, depth = 0):
 #	print depth*"-" + "\x1b[01m" + menu.getName().encode("ascii", 'ignore') + "\x1b[0m"
-	depth += 1
+#	depth += 1
 	for entry in menu.getEntries():
 		if isinstance(entry, xdg.Menu.Menu):
 			show_menu(entry, depth)
@@ -18,7 +18,7 @@ def show_menu(menu, depth = 0):
 #			print depth*"-" + "|||"
 #		elif isinstance(entry, xdg.Menu.Header):
 #			print depth*"-" + "\x1b[01m" + entry.Name + "\x1b[0m"
-	depth -= 1
+#	depth -= 1
 
 try:
 	menu = xdg.Menu.parse(sys.argv[1])
