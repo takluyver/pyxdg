6ecde500727dff8a906c80263fd4df92bec6ae3e
Heinrich Wendel <h_wendel@cojobo.net>
2005-03-21 09:40:04 +0000

    update ChangeLog

--- test/test-menu.py
+++ test/test-menu.py
@@ -6,24 +6,20 @@ import xdg.Menu
 import xdg.DesktopEntry
 
 def show_menu(menu, depth = 0):
-#	print depth*"-" + "\x1b[01m" + menu.getName().encode("ascii", 'ignore') + "\x1b[0m"
-#	depth += 1
+	print depth*"-" + "\x1b[01m" + menu.getName().encode("ascii", 'ignore') + "\x1b[0m"
+	depth += 1
 	for entry in menu.getEntries():
 		if isinstance(entry, xdg.Menu.Menu):
 			show_menu(entry, depth)
 		elif isinstance(entry, xdg.Menu.MenuEntry):
-#			print depth*"-" + entry.DesktopEntry.getName().encode("ascii", 'ignore')
-			print menu.getPath() + "/\t" + entry.DesktopFileID + "\t" + entry.DesktopEntry.getFileName()
-#		elif isinstance(entry, xdg.Menu.Separator):
-#			print depth*"-" + "|||"
-#		elif isinstance(entry, xdg.Menu.Header):
-#			print depth*"-" + "\x1b[01m" + entry.Name + "\x1b[0m"
-#	depth -= 1
+			print depth*"-" + entry.DesktopEntry.getName().encode("ascii", 'ignore')
+#			print menu.getPath() + "/\t" + entry.DesktopFileID + "\t" + entry.DesktopEntry.getFileName()
+		elif isinstance(entry, xdg.Menu.Separator):
+			print depth*"-" + "|||"
+		elif isinstance(entry, xdg.Menu.Header):
+			print depth*"-" + "\x1b[01m" + entry.Name + "\x1b[0m"
+	depth -= 1
 
-try:
-	menu = xdg.Menu.parse(sys.argv[1])
-#	menu.setWM("GNOME")
-#	menu.setLocale("de")
-	show_menu(menu)
-except IndexError:
-	show_menu(xdg.Menu.parse(), 0)
+#import profile
+#profile.run("xdg.Menu.parse()")
+show_menu(xdg.Menu.parse())
