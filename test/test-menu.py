dcd8a60f2f1724d4b481b7d61cf94c10a6977a51
Heinrich Wendel <h_wendel@cojobo.net>
2005-05-23 17:24:34 +0000

    update changelog

--- test/test-menu.py
+++ test/test-menu.py
@@ -6,19 +6,19 @@ import xdg.Menu
 import xdg.DesktopEntry
 
 def show_menu(menu, depth = 0):
-#	print depth*"-" + "\x1b[01m" + menu.getName().encode("ascii", 'ignore') + "\x1b[0m"
-#	depth += 1
+	print depth*"-" + "\x1b[01m" + menu.getName().encode("ascii", 'ignore') + "\x1b[0m"
+	depth += 1
 	for entry in menu.getEntries():
 		if isinstance(entry, xdg.Menu.Menu):
 			show_menu(entry, depth)
 		elif isinstance(entry, xdg.Menu.MenuEntry):
-#			print depth*"-" + entry.DesktopEntry.getName().encode("ascii", 'ignore')
-			print menu.getPath() + "/\t" + entry.DesktopFileID + "\t" + entry.DesktopEntry.getFileName()
+			print depth*"-" + entry.DesktopEntry.getName().encode("ascii", 'ignore')
+#			print menu.getPath() + "/\t" + entry.DesktopFileID + "\t" + entry.DesktopEntry.getFileName()
 #		elif isinstance(entry, xdg.Menu.Separator):
 #			print depth*"-" + "|||"
 #		elif isinstance(entry, xdg.Menu.Header):
 #			print depth*"-" + "\x1b[01m" + entry.Name + "\x1b[0m"
-#	depth -= 1
+	depth -= 1
 
 show_menu(xdg.Menu.parse())
 #xdg.Menu.parse()
