e379c2ea4e7f079143451976b5e0a6d7662f098d
Heinrich Wendel <h_wendel@cojobo.net>
2008-08-10 15:52:55 +0000

    fix wrong indention

--- xdg/DesktopEntry.py
+++ xdg/DesktopEntry.py
@@ -181,7 +181,7 @@ class DesktopEntry(IniFile):
         else:
             #OnlyShowIn and NotShowIn
             if self.content[group].has_key("OnlyShowIn") and self.content[group].has_key("NotShowIn"):
-            self.errors.append("Group may either have OnlyShowIn or NotShowIn, but not both")
+                self.errors.append("Group may either have OnlyShowIn or NotShowIn, but not both")
 
     def checkKey(self, key, value, group):
         # standard keys     
