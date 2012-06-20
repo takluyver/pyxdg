39fce62a480445f2b974a17064022f688143ba1c
Thomas Kluyver <takowl@gmail.com>
2012-06-20 21:13:49 +0100

    Update setup.py file

--- setup.py
+++ setup.py
@@ -7,7 +7,14 @@ setup( name = "pyxdg",
        description = "PyXDG contains implementations of freedesktop.org standards in python.",
        maintainer = "Freedesktop.org",
        maintainer_email = "xdg@lists.freedesktop.org",
-       url = "http://cvs.freedesktop.org/cgi-bin/viewcvs.cgi/pyxdg/",
+       url = "http://freedesktop.org/wiki/Software/pyxdg",
        packages = ['xdg'],
-       license = "LGPL-2")
+       classifiers = [
+                "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+                "Programming Language :: Python :: 2.6",
+                "Programming Language :: Python :: 2.7",
+                "Programming Language :: Python :: 3",
+                "Topic :: Desktop Environment",
+                ],
+     )
 
