00d0d94829001cda972a1db3d81e6f641f358d5c
Thomas Kluyver <takowl@gmail.com>
2012-07-25 00:23:16 +0100

    Release 0.21

--- xdg/__init__.py
+++ xdg/__init__.py
@@ -1,3 +1,3 @@
 __all__ = [ "BaseDirectory", "DesktopEntry", "Menu", "Exceptions", "IniFile", "IconTheme", "Locale", "Config", "Mime", "RecentFiles", "MenuEditor" ]
 
-__version__ = "0.20"
+__version__ = "0.21"
