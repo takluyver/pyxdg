cadd68bbf768d1cc853b365d0137c6038c03bc19
Thomas Kluyver <thomas@kluyver.me.uk>
2018-02-02 17:35:48 +0000

    Version number -> 0.26

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup( name = "pyxdg",
-       version = "0.25",
+       version = "0.26",
        description = "PyXDG contains implementations of freedesktop.org standards in python.",
        maintainer = "Freedesktop.org",
        maintainer_email = "xdg@lists.freedesktop.org",
