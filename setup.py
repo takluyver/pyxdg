30cc9ffdd2ade3b5c242ef3e34cefa0c63ff084f
Zac Bentley <zbbentley@gmail.com>
2022-07-14 14:37:11 -0400

    Drop support for Python 2; prevent errors due to race condition with existing directories

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@
 
 from setuptools import setup
 
-setup( name = "pyxdg",
-       version = "0.28",
-       description = "PyXDG contains implementations of freedesktop.org standards in python.",
-       maintainer = "Freedesktop.org",
-       maintainer_email = "xdg@lists.freedesktop.org",
-       url = "http://freedesktop.org/wiki/Software/pyxdg",
-       packages = ['xdg'],
-       classifiers = [
-            "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Topic :: Desktop Environment",
-       ],
+setup(
+    name="pyxdg",
+    version="0.29",
+    description="PyXDG contains implementations of freedesktop.org standards in python.",
+    maintainer="Freedesktop.org",
+    maintainer_email="xdg@lists.freedesktop.org",
+    url="http://freedesktop.org/wiki/Software/pyxdg",
+    packages=["xdg"],
+    python_requires=">=3.2",
+    classifiers=[
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Programming Language :: Python :: 3",
+        "Topic :: Desktop Environment",
+    ],
 )
-
